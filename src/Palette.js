861d2db89a7d0a9380abf6b9f6e37e6711c03ea4
gitburd <denaburd@gmail.com>
2020-06-13 00:14:00 -0700

    created react app and palette component

---
+++ src/Palette.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Palette extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    render() {
+        return (
+            <div className='Palette'>
+                <div className='Palette-colors'>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Palette
